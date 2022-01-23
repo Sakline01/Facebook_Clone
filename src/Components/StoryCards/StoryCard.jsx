c029286afba42ec0c18f0cc049a3330b65d90449
Sakline01 <mdsaklinesk@gmail.com>
2022-01-23 23:48:29 +0530

    completed the nav without search bar and added the active class css functinality

---
+++ src/Components/StoryCards/StoryCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StoryCard = () => {
+    return (
+        <div>
+            <h1>
+                hi from story card
+            </h1>
+        </div>
+    );
+};
+
+export default StoryCard;
